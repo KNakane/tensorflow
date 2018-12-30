89ae8cb8a43737d010a655cb6c4a4171b366db6d
KNakane <koki.bsb@gmail.com>
2018-12-30 12:27:25 +0900

    [update] rename and add AutoEncoder class

---
+++ AutoEncoder/train.py
@@ -0,0 +1,55 @@
+import sys
+sys.path.append('./utility')
+sys.path.append('./network')
+sys.path.append('./dataset')
+import tensorflow as tf
+from ae import AutoEncoder
+from AE_load import AE_Load
+from trainer import Train
+from collections import OrderedDict
+
+
+def set_model(outdim):
+    encode = [['conv', 5, 32, 1, tf.nn.relu],
+              ['max_pool', 2, 2],
+              ['conv', 5, 64, 1, tf.nn.relu],
+              ['max_pool', 2, 2]]
+
+    decode = [['deconv',  5, 32, 2, tf.nn.relu],
+              ['deconv',  5, outdim, 2, None]]
+    return encode, decode
+
+def main(argv):
+    message = OrderedDict({
+        "Network": 'AutoEncoder',
+        "data": FLAGS.data,
+        "epoch":FLAGS.n_epoch,
+        "batch_size": FLAGS.batch_size,
+        "Optimizer":FLAGS.opt,
+        "learning_rate":FLAGS.lr})
+
+    # prepare training
+    ## load dataset
+    data = AE_Load(FLAGS.data)
+    ## setting models
+    encode, decode = set_model(data.channel)
+    model = AutoEncoder(encode=encode, decode=decode, name='AutoEncoder', out_dim=data.output_dim, lr=FLAGS.lr, opt=FLAGS.opt, trainable=True)
+
+    #training
+    trainer = Train(FLAGS, message, data, model, 'AutoEncoder')
+    trainer.train()
+    
+    return
+
+if __name__ == '__main__':
+    flags = tf.app.flags
+    FLAGS = flags.FLAGS
+    flags.DEFINE_string('data', 'mnist', 'Choice the training data name -> ["mnist","cifar10","cifar100","kuzushiji"]')
+    flags.DEFINE_integer('n_epoch', '1000', 'Input max epoch')
+    flags.DEFINE_integer('batch_size', '32', 'Input batch size')
+    flags.DEFINE_float('lr', '0.001', 'Input learning rate')
+    flags.DEFINE_string('opt', 'SGD', 'Choice the optimizer -> ["SGD","Momentum","Adadelta","Adagrad","Adam","RMSProp"]')
+    flags.DEFINE_integer('checkpoints_to_keep', 5,'checkpoint keep count')
+    flags.DEFINE_integer('keep_checkpoint_every_n_hours', 1, 'checkpoint create ')
+    flags.DEFINE_integer('save_checkpoint_steps', 1000,'save checkpoint step')
+    tf.app.run()
---
+++ network/ae.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import os,sys
+sys.path.append('./utility')
+sys.path.append('./network')
+import tensorflow as tf
+from cnn import CNN
+from module import Module
+from optimizer import *
+
+class AutoEncoder(CNN):
+    def __init__(self, 
+                 encode=None,
+                 decode=None,
+                 name='AutoEncoder',
+                 out_dim=10,
+                 opt=Adam,   # Choice the optimizer -> ["SGD","Momentum","Adadelta","Adagrad","Adam","RMSProp"]
+                 lr=0.001,
+                 trainable=False
+                 ):
+        assert encode is not None, "Please set encode model"
+        assert decode is not None, "Please set decode model"
+        super().__init__(name=name, out_dim=out_dim, opt=opt, lr=lr, trainable=trainable)
+        self.encode = encode
+        self.decode = decode
+
+    def Encode(self, outputs):
+        with tf.variable_scope('Encode'):
+            for l in range(len(self.encode)):
+                outputs = (eval('self.' + self.encode[l][0])(outputs, self.encode[l][1:]))
+            return outputs
+    
+    def Decode(self, outputs):
+        with tf.variable_scope('Decode'):
+            for l in range(len(self.decode)):
+                outputs = (eval('self.' + self.decode[l][0])(outputs, self.decode[l][1:]))
+            return outputs
+
+    def variable(self, outputs):
+        return outputs
+
+    def inference(self, outputs):
+        with tf.variable_scope(self.name):
+            outputs = self.Encode(outputs)
+            if self.name == 'VAE':
+                outputs = self.varialble(outputs)
+            outputs = self.Decode(outputs)
+            return outputs
+        
+    def loss(self, logits, labels):
+        loss = tf.reduce_mean(tf.square(logits - labels))
+        return loss
