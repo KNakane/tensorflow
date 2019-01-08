b471fd163469205dad75729d0940216c99ff14b2
k_nakane <Nakane.Koki@cw.MitsubishiElectric.co.jp>
2019-01-08 09:59:05 +0900

    [fix] optuna

--- optuna/search.py
+++ optuna/search.py
@@ -10,21 +10,28 @@ from functools import partial
 class Optuna():
     def __init__(self, db_name=None):
         self.name = db_name if db_name is not None else 'example'
-        self.util = Utils(prefix='optuna')
-        self.util.conf_log()
 
     def search(self, obj, para, trials):
         assert trials > 0, "trial is bigger than 0"
-
-        study = optuna.create_study(study_name=self.name, storage='sqlite:///{}/hypara_search.db'.format(self.util.res_dir))
+        util = Utils(prefix='optuna')
+        util.conf_log()
+        study = optuna.create_study(study_name=self.name, storage='sqlite:///{}/hypara_search.db'.format(util.res_dir))
         f = partial(obj, para)
         study.optimize(f, n_trials=trials)
         return
 
     def confirm(self, directory):
-        study = optuna.Study(study_name=self.name, storage='sqlite:///{}/hypara_search.db'.format(directory))
-        df = study.trials_dataframe()
-        study.best_params  # Get best parameters for the objective function.
-        study.best_value  # Get best objective value.
-        study.best_trial  # Get best trial's information.
-        study.trials  # Get all trials' information.
+        self.study = optuna.Study(study_name=self.name, storage='sqlite:///{}/hypara_search.db'.format(directory))
+        self.df = self.study.trials_dataframe()
+        """
+        self.study.best_params  # Get best parameters for the objective function.
+        self.study.best_value  # Get best objective value.
+        self.study.best_trial  # Get best trial's information.
+        self.study.trials  # Get all trials' information.
+        """
+        
+if __name__ == '__main__':
+    op = Optuna('example-study')
+    op.confirm('results')
+    print(op.study.best_params)
+    print(op.study.best_value)
