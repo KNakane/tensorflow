c6d4977a52fa0bcc73aec3b0b02f8aaaa57416a9
Koki Nakane <k_nakane@mbp02.isl.melco.co.jp>
2019-01-10 11:48:42 +0900

    [update] add shift & rotate in Augmentation

--- dataset/Augmentation.py
+++ dataset/Augmentation.py
@@ -11,7 +11,7 @@ class Augment():
         self.aug_img = self.img.copy()
         self.aug_label = self.label.copy() 
 
-    def shift(self, v, h):
+    def shift(self, v=3, h=3):
         aug = []
         for i in trange(self.img.shape[0], desc="Augmentation -> Shift"):
             vertical = 2*v * np.random.rand() - v
@@ -32,7 +32,7 @@ class Augment():
     def rotate(self, angle_range=(-20, 20)):
         aug = []
         for i in trange(self.img.shape[0], desc="Augmentation -> Rotate"):
-            h, w, _ = self.img[i].shape
+            h, w = self.img[i].shape[0], self.img[i].shape[1]
             angle = np.random.randint(*angle_range)
             image = rotate(self.img[i], angle)
             aug.append(imresize(image, (h, w)))
@@ -40,9 +40,19 @@ class Augment():
         self.aug_label = np.hstack((self.aug_label, self.label))
         return self.aug_img, self.aug_label
 
-    def shift_rotate(self, v, h, angle_range=(-20, 20)):   
-        pass
-        return
+    def shift_rotate(self, v=3, h=3, angle_range=(-20, 20)):
+        aug = []
+        for i in trange(self.img.shape[0], desc="Augmentation -> Shift & Rotate"):
+            vertical = 2*v * np.random.rand() - v
+            horizontal = 2*h * np.random.rand() - h
+            self.img[i] = shift(self.img[i], [vertical, horizontal], cval=0)
+            h, w = self.img[i].shape[0], self.img[i].shape[1]
+            angle = np.random.randint(*angle_range)
+            image = rotate(self.img[i], angle)
+            aug.append(imresize(image, (h, w)))
+        self.aug_img = np.vstack((self.aug_img, np.asarray(aug)))
+        self.aug_label = np.hstack((self.aug_label, self.label))
+        return self.aug_img, self.aug_label
 
     def cutout(self):
         #reference : https://arxiv.org/abs/1708.04552 or http://kenbo.hatenablog.com/entry/2017/11/28/211932
