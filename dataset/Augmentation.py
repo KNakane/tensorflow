0f65e1aa73a01bbb9bb04d165a847690d4ff12f9
Koki Nakane <k_nakane@mbp02.isl.melco.co.jp>
2019-01-10 09:13:07 +0900

    [update] add method in Augmentation

--- dataset/Augmentation.py
+++ dataset/Augmentation.py
@@ -1,7 +1,8 @@
 import os, sys
 import numpy as np
 from tqdm import trange
-from scipy.ndimage.interpolation import shift
+from scipy.misc import imresize
+from scipy.ndimage.interpolation import shift, rotate
 
 class Augment():
     def __init__(self, images, labels):
@@ -12,7 +13,7 @@ class Augment():
 
     def shift(self, v, h):
         aug = []
-        for i in trange(self.img.shape[0], desc="Augmentation -> shift"):
+        for i in trange(self.img.shape[0], desc="Augmentation -> Shift"):
             vertical = 2*v * np.random.rand() - v
             horizontal = 2*h * np.random.rand() - h
             aug.append(shift(self.img[i], [vertical, horizontal], cval=0))
@@ -21,5 +22,29 @@ class Augment():
         return self.aug_img, self.aug_label
 
     def mirror(self):
+        aug = []
+        for i in trange(self.img.shape[0], desc="Augmentation -> Mirror"):
+            aug.append(self.img[i, :, ::-1, :])
+        self.aug_img = np.vstack((self.aug_img, np.asarray(aug)))
+        self.aug_label = np.hstack((self.aug_label, self.label))
+        return self.aug_img, self.aug_label
+
+    def rotate(self, angle_range=(-20, 20)):
+        aug = []
+        for i in trange(self.img.shape[0], desc="Augmentation -> Rotate"):
+            h, w, _ = self.img[i].shape
+            angle = np.random.randint(*angle_range)
+            image = rotate(self.img[i], angle)
+            aug.append(imresize(image, (h, w)))
+        self.aug_img = np.vstack((self.aug_img, np.asarray(aug)))
+        self.aug_label = np.hstack((self.aug_label, self.label))
+        return self.aug_img, self.aug_label
+
+    def shift_rotate(self, v, h, angle_range=(-20, 20)):   
         pass
-        #return np.array(self.aug_img), np.array(self.aug_label)
+        return
+
+    def cutout(self):
+        #reference : https://arxiv.org/abs/1708.04552 or http://kenbo.hatenablog.com/entry/2017/11/28/211932
+        pass
+        return
