a6c73c358d73fd5b7927b0b81690b76e29a42d32
KNakane <koki.bsb@gmail.com>
2019-01-14 01:17:43 +0900

    [update] ddpg

--- rl/agents/ddpg.py
+++ rl/agents/ddpg.py
@@ -5,25 +5,51 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../../network'))
 import numpy as np
 import tensorflow as tf
 from optimizer import *
+from dqn import DQN
 from cnn import CNN
 from critic_net import Critic_Net
 from writer import Writer
-from dqn import DQN
+from OU_noise import OrnsteinUhlenbeckProcess
 
-class DDPG():
-    def __init__(self):
-        self.actor = Actor()
+
+class DDPG(DQN):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.actor_model, self.critic_model = kwargs['model']
+        self.actor = Actor(model=self.actor_model, )
         self.critic = Critic()
 
-    def update(self, replay_data):
+        self.noise = OrnsteinUhlenbeckProcess(num_actions=self.n_actions)
 
+        self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
+        self.writer = Writer(self.sess)
+
+    def choose_action(self, observation):
+        # to have batch dimension when feed into tf placeholder
+        observation = observation[np.newaxis, :]
+
+        if np.random.uniform() < self.epsilon:
+            # forward feed the observation and get q value for every actions
+            actions_value = self.sess.run(self.q_eval, feed_dict={self.s: observation})
+            action = np.argmax(actions_value)
+        else:
+            action = np.random.randint(0, self.n_actions)
+        return action + self.noise.generate()
+
+    def update(self, replay_data):
+        # Prepare minibatch sampling from replay buffer
         bs, ba, done, bs_, br = replay_data
 
         #target_actor session run
+        action = self.sess.run(self.actor.q_next, feed_dict={self.s_:bs_})
         q_next, q_eval = self.sess.run([self.critic.q_next, self.critic.q_eval],
-                feed_dict = {self.s_:bs_,
-                            self.a_:actor.q_next})
+                                        feed_dict = {self.critic.s_: bs_,
+                                                     self.critic.a_: action,
+                                                     self.critic.s: bs,
+                                                     self.critic.a: ba})
 
+        # put result in q_target[]
         q_target = q_eval.copy()
 
         batch_index = np.arange(self.batch_size, dtype=np.int32)
@@ -33,18 +59,32 @@ class DDPG():
 
         q_target[batch_index, eval_act_index] = reward + self.gamma * np.max(q_next, axis=1) * (1. - done)
 
-        # train eval network
-        merged, _, self.cost = self.sess.run([self.merged, self.critic._train_op, self.loss],
-                                     feed_dict={self.s: bs,
-                                                self.q_target: q_target})
+        # train critic net
+        _ = self.sess.run(self.critic.update_q_net, 
+                                feed_dict={self.critic.s: bs,
+                                           self.critic.a: ba,
+                                           self.critic.q_target: q_target})
 
+        # calculate policy gradient and train actor net
+        action_batch_for_gradients = self.sess.run(self.actor.q_eval, feed_dict={self.actor.s:bs})
+        q_gradient_batch = self.sess.run(self.critic.action_gradients,
+                                                    feed_dict = {self.critic.s: bs,
+                                                                 self.critic.a: action_batch_for_gradients})
 
-    def update_critic(self, ):
-        self.critic.update_q_net()
+        self.merged = tf.summary.merge_all()
+        _, merged = self.sess.run([self.actor.update_q_net,self.merged], 
+                                            feed_dict={self.actor.q_target:q_gradient_batch,
+                                                       self.actor.s :bs})
 
-    def update_actor(self):
-        self.actor.update_q_net()
+        # check to replace target parameters
+        if self._iteration % self.replace_target_iter == 0:
+            self.sess.run([self.critic.replace_target_op,self.actor.replace_target_op])
 
+        # increasing epsilon
+        self.epsilon = self.epsilon + self.epsilon_increment if self.epsilon < self.epsilon_max else self.epsilon_max
+
+        self._iteration += 1
+        self.writer.add(merged, self._iteration)
 
 
 class Actor(DQN):
@@ -52,6 +92,7 @@ class Actor(DQN):
         super().__init__(*args, **kwargs)
 
         e_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='eval_net/Actor')
+        self.eval_param = e_params
         assert len(e_params) > 0
         t_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='target_net/target_Actor')
         assert len(t_params) > 0
@@ -62,6 +103,12 @@ class Actor(DQN):
     def _build_net(self):
         with tf.variable_scope('eval_net'):
             self.q_eval_model = CNN(model=self.model, name='Actor', opt=self._optimizer, lr=self.lr, trainable=True)
+
+            with tf.variable_scope('parameters_gradients'):
+                self.parameters_gradients = tf.gradients(self.q_eval, self.eval_param, -self.q_target)
+            
+            with tf.variable_scope('train'):
+                self.update_q_net = self.q_eval_model.optimizer.apply_gradients(zip(self.parameters_gradients,self.eval_param))
 
         with tf.variable_scope('target_net'):
             self.q_next = CNN(model=self.model, name='target_Actor', trainable=False).inference(self.s_)
@@ -82,16 +129,23 @@ class Critic(DQN):
         with tf.variable_scope('replace_op'):
             self.replace_target_op = [tf.assign(t, self.tau * e + (1 - self.tau) * t) for t, e in zip(t_params, e_params)]
 
-        with tf.variable_scope('action_gradients'):
-            self.action_gradients = tf.gradients(self.q_eval, self.a)
 
     def _build_net(self):
         with tf.variable_scope('eval_net'):
             self.q_eval_model = Critic_Net(model=self.model, name='Critic', opt=self._optimizer, lr=self.lr, trainable=True)
             self.q_eval = self.q_eval_model.inference(self.s, self.a)
 
-        with tf.variable_scope('target_net'):
-            self.q_next = Critic_Net(model=self.model, name='target_Critic', trainable=False).inference(self.s_, self.a)
+            with tf.variable_scope('action_gradients'):
+                self.action_gradients = tf.gradients(self.q_eval, self.a)
 
-    def update_q_net(self):
-        return
+            with tf.variable_scope('loss'):
+                self.loss = tf.losses.huber_loss(labels=self.q_target, predictions=self.q_eval)
+                #self.loss = tf.reduce_mean(tf.squared_difference(self.q_target, self.q_eval))
+                self.loss_summary = tf.summary.scalar("loss", self.loss)
+            
+            with tf.variable_scope('train'):
+                self.update_q_net = self.q_eval_model.optimize(self.loss)
+
+        with tf.variable_scope('target_net'):
+            self.q_next = Critic_Net(model=self.model, name='target_Critic', trainable=False).inference(self.s_, self.a_)
+            
