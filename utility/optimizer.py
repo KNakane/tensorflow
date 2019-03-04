82c03c6cbf27febc9e11714cabd146064f41c56a
Koki Nakane <k_nakane@mbp02.isl.melco.co.jp>
2019-03-04 14:22:51 +0900

    [fix] remove minimize in each optimizer

--- utility/optimizer.py
+++ utility/optimizer.py
@@ -8,7 +8,7 @@ class SGD():
         self.lr = tf.train.exponential_decay(learning_rate,global_step,decay_step,decay_rate,staircase=True)
         self.method = tf.train.GradientDescentOptimizer(self.lr)
 
-    def optimize(self, global_step, loss=None, var_list=None):
+    def optimize(self, global_step=None, loss=None, var_list=None):
         if loss is None:
             NotImplementedError()
         else:
@@ -20,17 +20,11 @@ class Momentum(SGD):
         super().__init__(learning_rate=learning_rate, decay_step=decay_step, decay_rate=decay_rate)
         self.method = tf.train.MomentumOptimizer(learning_rate=self.lr, momentum=0.9)
 
-    def optimize(self, global_step, loss=None, var_list=None):
-        return self.method.minimize(loss, global_step, var_list)
-
 
 class Adadelta(SGD):
     def __init__(self, learning_rate=0.1, decay_step=1000, decay_rate=0.95):
         super().__init__(learning_rate=learning_rate, decay_step=decay_step, decay_rate=decay_rate)
         self.method = tf.train.AdadeltaOptimizer(self.lr)
-
-    def optimize(self, global_step, loss=None, var_list=None):
-        return self.method.minimize(loss, global_step, var_list)
 
 
 class Adagrad(SGD):
@@ -38,23 +32,14 @@ class Adagrad(SGD):
         super().__init__(learning_rate=learning_rate, decay_step=decay_step, decay_rate=decay_rate)
         self.method = tf.train.AdagradOptimizer(self.lr)
 
-    def optimize(self, global_step, loss=None, var_list=None):
-        return self.method.minimize(loss, global_step, var_list)
-
 
 class Adam(SGD):
     def __init__(self, learning_rate=0.1, decay_step=1000, decay_rate=0.95):
         super().__init__(learning_rate=learning_rate, decay_step=decay_step, decay_rate=decay_rate)
         self.method = tf.train.AdamOptimizer(self.lr, beta1=0.5)
 
-    def optimize(self, global_step, loss=None, var_list=None):
-        return self.method.minimize(loss, global_step, var_list)
-
 
 class RMSProp(SGD):
     def __init__(self, learning_rate=0.1, decay_step=1000, decay_rate=0.95):
         super().__init__(learning_rate=learning_rate, decay_step=decay_step, decay_rate=decay_rate)
         self.method = tf.train.RMSPropOptimizer(self.lr)
-
-    def optimize(self, global_step, loss=None, var_list=None):
-        return self.method.minimize(loss, global_step, var_list)
