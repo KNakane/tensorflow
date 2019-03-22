8a76d445a710cbf415260f49b3f419ebea9314a5
KNakane <koki.bsb@gmail.com>
2019-03-22 18:20:10 +0900

    [update] make graph by tensorboard

--- utility/event_getter.py
+++ utility/event_getter.py
@@ -1,4 +1,5 @@
 import os, sys
+import re
 import glob
 import datetime
 import argparse
@@ -15,8 +16,9 @@ class EventGetter():
         dt_now = datetime.datetime.now()
         self.events_list = events_list
         self.log_dir = "results/" + dt_now.strftime("%y%m%d_%H%M%S") + "_events"
+        self.result_dic = {}
         self.name_list = []
-        #self._init_log()
+        self._init_log()
 
     def _init_log(self):
         """ 結果を格納するdirectoryの作成 """
@@ -25,10 +27,11 @@ class EventGetter():
         tf.gfile.MakeDirs(self.log_dir)
 
     def __call__(self):
-        res_list = self.gather_result()
-        for key in res_list[0].keys():
-            for i in range(len(res_list)):
-                self.make_graph(key, res_list[i][key])
+        # eventごとにデータを取得し、dictに格納する
+        self.gather_result()
+        # 格納したdictから各項目ごとにグラフを作成する
+        for key in self.result_dic:
+            self.make_graph(key, self.result_dic[key])
         #self.logger()
         return
     
@@ -40,80 +43,69 @@ class EventGetter():
         return
 
     def gather_result(self):
-        tmp_res = []
-        old_key = None
+        """
+        directoryの中身を取得、項目ごとにdictに格納
+        """
         for event in self.events_list:
-            result_dic = self.get_scores(event)
-            if result_dic is None:
-                continue
-            else:
-                if old_key is None:
-                    tmp_res.append(result_dic)
-                    old_key = result_dic.keys()
-                    continue
-                elif set(list(old_key)) == set(list(result_dic.keys())):
-                    tmp_res.append(result_dic)
-                    old_key = result_dic.keys()
-                else:
-                    raise NotImplementedError()
-        return tmp_res
+            # 名前の取得
+            name = event.split('/')[1]
+            self.get_scores(name, event)
+        return
         
-    def get_scores(self, path_file):
+    def get_scores(self, name, path_file):
         """指定されたファイルからスコアの系列を取得して返す
 
-        :param path_file:
-        :return:
-        (steps, scores)
-        steps : ステップの系列
-        scores : スコアの系列
+         parameters
+        ----------
+        name : result directory
+
+        path_file : event path
+
+        returns
+        ----------
+        
         """
-        dic = {} # 結果を格納するdictionary
         accumulator = EventAccumulator(path_file)
         accumulator.Reload()
 
         tag_dict = accumulator.Tags() #'images', 'audio', 'histograms', 'scalars', 'distributions', 'tensors', 'graph', 'meta_graph', 'run_metadata'
         scalars_key = tag_dict['scalars']
-        if not scalars_key:
-            return None
+        assert scalars_key is not None
 
-        self.name_list.append(path_file.split('/')[1])
         for key in scalars_key:
-            eval = accumulator.Scalars(key)
-            if 'steps' not in dic:
-                dic['steps'] = np.array([tmp.step for tmp in eval])
-            dic[key] = np.array([tmp.value for tmp in eval]) 
+            key_name = key.replace('/','_')
+            if not key_name in self.result_dic:
+                self.result_dic.setdefault(key_name,{})
+            if not name in self.result_dic[key_name]:
+                self.result_dic.setdefault(name,{})
+            value = accumulator.Scalars(key)
+            self.result_dic[key_name][name] = np.array([tmp.value for tmp in value])
+        return 
 
-        return dic
-
-    def make_graph(self, key, values):
-        fig = plt.figure(figsize=(10,3))
+    def make_graph(self, name, values):
+        fig = plt.figure(figsize=(10,5))
         colorlist = ["r", "g", "b", "c", "m", "y", "k", "w"]
-        ax = fig.add_subplot(1, 2, 1)
-        tmp = np.zeros((len(values), values[0].shape[0]))
-        for i,value in enumerate(values):
-            tmp[i] = value
-            plt.plot(range(value.shape[0]), value, linestyle='solid', color=colorlist[i], label=i)
-        ax.yaxis.set_major_formatter(ticker.PercentFormatter(xmax=1.0))
+        ax = fig.add_subplot(1, 1, 1)
+        i = 0
+        for key in values:
+            if key is None:
+                continue
+            array = values[key]
+            plt.plot(range(array.shape[0]), array, linestyle='solid', color=colorlist[i], label=key)
+            i += 1
+        if re.search('accuracy', name):
+            ax.yaxis.set_major_formatter(ticker.PercentFormatter(xmax=1.0))
         plt.grid(which='major',color='gray',linestyle='-')
-        plt.title("FetchReach-v1 of Each Result")
-        plt.xlabel("Episode")
-        plt.ylabel("Success Rate")
-
-        ax = fig.add_subplot(1, 2, 2)
-        mean = np.mean(tmp, axis=0)
-        std = np.std(tmp, axis=0)
-        plt.grid(which='major',color='gray',linestyle='-')
-        plt.plot(range(tmp.shape[1]), mean, linestyle='solid', color='blue', label=i)
-        plt.fill_between(range(tmp.shape[1]) ,mean - std,mean + std,facecolor='blue',alpha=0.3)
-        ax.yaxis.set_major_formatter(ticker.PercentFormatter(xmax=1.0))
-        plt.title("FetchReach-v1 of Average Result")
-        plt.xlabel("Episode")
-        plt.ylabel("Success Rate")
-        plt.savefig(self.log_dir + '/accuracy.png')
+        plt.title(name)
+        plt.xlabel("epoch")
+        plt.ylabel("value")
+        plt.legend()
+        plt.savefig(self.log_dir + '/{}.png'.format(name))
         return
 
 def main(args):
-    events_list = list(chain.from_iterable([glob.glob(res_dir+"/**/events.*", recursive=True) for res_dir in args.dir]))
+    assert args.dir is not None
+    events_list = list(chain.from_iterable([glob.glob(res_dir+"tf_board/events.*", recursive=True) for res_dir in args.dir]))
     evget = EventGetter(events_list)
     evget()
     return
