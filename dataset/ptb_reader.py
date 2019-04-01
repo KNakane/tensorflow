9c11a118914ee2a66e4726dabee6af91d4545a01
Koki Nakane <k_nakane@mbp02.isl.melco.co.jp>
2019-04-01 15:36:39 +0900

    [add] ptb_reader

---
+++ dataset/ptb_reader.py
@@ -0,0 +1,115 @@
+"""Utilities for parsing PTB text files."""
+# based on https://github.com/tensorflow/models/blob/master/tutorials/rnn/ptb/reader.py
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import collections
+import os
+import sys
+import tensorflow as tf
+
+
+Py3 = sys.version_info[0] == 3
+
+def _read_words(filename):
+    with tf.gfile.GFile(filename, "r") as f:
+        if Py3:
+            return f.read().replace("\n", "<eos>").split()
+        else:
+            return f.read().decode("utf-8").replace("\n", "<eos>").split()
+
+def _file_to_word_ids(filename, word_to_id):
+    data = _read_words(filename)
+    return [word_to_id[word] for word in data if word in word_to_id]
+
+def _build_vocab(filename):
+    data = _read_words(filename)
+
+    counter = collections.Counter(data)
+    count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
+
+    words, _ = list(zip(*count_pairs))
+    word_to_id = dict(zip(words, range(len(words))))
+
+    return word_to_id
+
+def ptb_raw_data(data_path=None):
+    train_path = os.path.join(data_path, "ptb.train.txt")
+    valid_path = os.path.join(data_path, "ptb.valid.txt")
+    test_path = os.path.join(data_path, "ptb.test.txt")
+    
+    word_to_id = _build_vocab(train_path)
+    train_data = _file_to_word_ids(train_path, word_to_id)
+    valid_data = _file_to_word_ids(valid_path, word_to_id)
+    test_data = _file_to_word_ids(test_path, word_to_id)
+    vocabulary = len(word_to_id)
+    return train_data, valid_data, test_data, vocabulary
+
+
+def ptb_producer(raw_data, batch_size, num_steps, name=None):
+    with tf.name_scope(name, "PTBProducer", [raw_data, batch_size, num_steps]):
+        raw_data = tf.convert_to_tensor(raw_data, name="raw_data", dtype=tf.int32)
+
+        data_len = tf.size(raw_data)
+        batch_len = data_len // batch_size
+        data = tf.reshape(raw_data[0 : batch_size * batch_len],
+                        [batch_size, batch_len])
+
+        epoch_size = (batch_len - 1) // num_steps
+        assertion = tf.assert_positive(
+            epoch_size,
+            message="epoch_size == 0, decrease batch_size or num_steps")
+        with tf.control_dependencies([assertion]):
+            epoch_size = tf.identity(epoch_size, name="epoch_size")
+
+            i = tf.train.range_input_producer(epoch_size, shuffle=False).dequeue()
+            x = tf.strided_slice(data, [0, i * num_steps],
+                                [batch_size, (i + 1) * num_steps])
+            x.set_shape([batch_size, num_steps])
+            y = tf.strided_slice(data, [0, i * num_steps + 1],
+                                [batch_size, (i + 1) * num_steps + 1])
+            y.set_shape([batch_size, num_steps])
+            return x, y
+
+
+def _retrieve_word_vocabulary():
+    def creator(path):
+        words = _load_words(_train_url)
+        vocab = {}
+        index = 0
+        with open(path, 'w') as f:
+            for word in words:
+                if word not in vocab:
+                    vocab[word] = index
+                    index += 1
+                    f.write(word + '\n')
+
+        return vocab
+
+    def loader(path):
+        vocab = {}
+        with open(path) as f:
+            for i, word in enumerate(f):
+                vocab[word.strip()] = i
+        return vocab
+
+    root = download.get_dataset_directory('pfnet/chainer/ptb')
+    path = os.path.join(root, 'vocab.txt')
+    return download.cache_or_load_file(path, creator, loader)
+
+def _retrieve_ptb_words(name, url):
+    def creator(path):
+        vocab = _retrieve_word_vocabulary()
+        words = _load_words(url)
+        x = numpy.empty(len(words), dtype=numpy.int32)
+        for i, word in enumerate(words):
+            x[i] = vocab[word]
+
+        numpy.savez_compressed(path, x=x)
+        return {'x': x}
+
+    root = download.get_dataset_directory('pfnet/chainer/ptb')
+    path = os.path.join(root, name)
+    loaded = download.cache_or_load_file(path, creator, numpy.load)
+    return loaded['x']
