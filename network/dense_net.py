6b3a84edf749556127958c64c8b1221b92a9c37c
Koki Nakane <k_nakane@mbp02.isl.melco.co.jp>
2019-04-08 15:43:12 +0900

    [fix] densenet

--- network/dense_net.py
+++ network/dense_net.py
@@ -24,7 +24,7 @@ class DenseNet(CNN):
         with tf.variable_scope(self.name):
             if reuse:
                 tf.get_variable_scope().reuse_variables()
-            featmap = self.conv(images, [7, self.growth_k * 2, 2, tf.nn.relu], name="init_conv")
+            featmap = self.conv(images, [7, self.growth_k * 2, 2, None], name="init_conv")
             featmap = self.max_pool(featmap, [3, 2, 'VALID'])
             for i in range(self.nb_blocks) :
                 featmap = self.dense_block(featmap, n_layers=4, bottle_neck=True, name='dense_'+str(i))
@@ -32,6 +32,7 @@ class DenseNet(CNN):
 
             featmap = self.dense_block(featmap, n_layers=31, bottle_neck=True, name='dense_final')
             featmap = self.ReLU(self.BN(featmap, [None]),[None])
+            featmap = self.gap(featmap, [self.out_dim])
             featmap = self.fc(featmap, [self.out_dim,None])
             logits  = tf.identity(featmap, name="output_logits")
 
@@ -46,12 +47,16 @@ class DenseNet(CNN):
                 if bottle_neck:
                     logits = self.ReLU(self.BN(x, [None]),[None])
                     logits = self.conv(logits, [1, self.growth_k * 4, 1, None])
+                    logits = tf.layers.dropout(inputs=logits, rate=0.2, training=self._trainable)
                     logits = self.ReLU(self.BN(logits, [None]),[None])
                     logits = self.conv(logits, [3, self.growth_k, 1, None])
+                    logits = tf.layers.dropout(inputs=logits, rate=0.2, training=self._trainable)
 
                 else:
                     logits = self.ReLU(self.BN(x, [None]),[None])
                     logits = self.conv(logits, [3, self.growth_k * 4, 1, None])
+                    logits = tf.layers.dropout(inputs=logits, rate=0.2, training=self._trainable)
+
                 layers_concat.append(logits)
                 x = tf.concat(layers_concat, axis=3)
             return x
@@ -61,5 +66,6 @@ class DenseNet(CNN):
             x = self.ReLU(self.BN(x, [None]),[None])
             in_channel = x.get_shape().as_list()[3]
             x = self.conv(x, [1, in_channel * 0.5, 1, None])
+            x = tf.layers.dropout(inputs=x, rate=0.2, training=self._trainable)
             x = self.avg_pool(x, [2, 2, 'VALID'])
             return x
