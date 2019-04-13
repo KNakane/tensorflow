697b814d9dc1ec133d72853028ff36bcccba364c
KNakane <koki.bsb@gmail.com>
2019-04-14 00:36:01 +0900

    [update] basedtrainer

--- rl/rl_trainer.py
+++ rl/rl_trainer.py
@@ -11,8 +11,7 @@ from utils import Utils
 from display_as_gif import display_frames_as_gif
 from replay_memory import ReplayBuffer,PrioritizeReplayBuffer
 
-
-class Trainer():
+class BasedTrainer():
     def __init__(self, 
                  agent, 
                  env, 
@@ -50,93 +49,130 @@ class Trainer():
         self.test_render = test_render
         self.test_frame = test_frame
         self.init_model_dir = init_model_dir
-    
+
     def train(self):
-        board_writer = tf.contrib.summary.create_file_writer(self.util.tf_board)
+        board_writer = self.begin_train()
         board_writer.set_as_default()
-        self.util.save_init(self.agent)
-        total_steps = 0
-        learning_flag = 0
+        self.total_steps = 0
+        self.learning_flag = 0
         for episode in range(1, self.n_episode+1):
             self.global_step.assign_add(1)
-            with tf.contrib.summary.always_record_summaries():
-                state = self.env.reset()
-                total_reward = 0
-                for step in range(1, self.max_steps+1):
-                    if self.render:
-                        self.env.render()
+            self.step(episode)
+        self.episode_end()
+        return
 
-                    action = self.agent.choose_action(state)
-                    state_, reward, done, _ = self.env.step(action)
+    def episode_begin(self, episode, agent):
+        pass
+    
+    def begin_train(self):
+        self.util.save_init(self.agent)
+        return tf.contrib.summary.create_file_writer(self.util.tf_board)
+    
+    def step(self, episode):
+        NotImplementedError()
 
-                    # Multi-step learning
-                    self.state_deque.append(state)
-                    self.reward_deque.append(reward)
-                    self.action_deque.append(action)
+    def step_end(self, episode, step, total_reward):
+        self.total_steps += step
+        tf.contrib.summary.scalar('train/total_steps', self.total_steps)
+        tf.contrib.summary.scalar('train/steps_per_episode', step)
+        tf.contrib.summary.scalar('train/total_reward', total_reward)
+        tf.contrib.summary.scalar('train/average_reward', total_reward / step)
+        print("episode: %d total_steps: %d  steps/episode: %d  total_reward: %0.2f"%(episode, self.total_steps, step, total_reward))
+        metrics = OrderedDict({
+            "episode": episode,
+            "total_steps": self.total_steps,
+            "steps/episode":step,
+            "total_reward": total_reward})
+        self.util.write_log(message=metrics)
+        self.util.save_model()
+        self.state_deque.clear()
+        self.action_deque.clear()
+        self.reward_deque.clear()
 
-                    # the smaller theta and closer to center the better
-                    if self.env.__class__.__name__ == 'CartPoleEnv':
-                        x, x_dot, theta, theta_dot = state_
-                        r1 = (self.env.x_threshold - abs(x))/self.env.x_threshold - 0.8
-                        r2 = (self.env.theta_threshold_radians - abs(theta))/self.env.theta_threshold_radians - 0.5
-                        reward = r1 + r2
-
-                    if len(self.state_deque) == self.multi_step or done:
-                        t_reward, p_index = self.multi_step_reward(self.reward_deque, self.agent.discount)
-                        state = self.state_deque[0]
-                        action = self.action_deque[0]
-                        self.replay_buf.push(state, action, done, state_, t_reward, p_index)
-
-                    total_reward += reward
-                    if len(self.replay_buf) > self.replay_size and len(self.replay_buf) > self.n_warmup:
-                        indexes, transitions, weights = self.replay_buf.sample(self.agent.batch_size, episode/self.n_episode)
-                        train_data = map(np.array, zip(*transitions))
-                        self.agent.update_q_net(train_data, weights)
-                        learning_flag = 1
-                        if len(self.agent.bs.shape) == 4:
-                            image = tf.expand_dims(self.agent.bs[:3,:,:,0],3)
-                            tf.contrib.summary.image('train/input_img', tf.cast(image * 255.0, tf.uint8))
-                        if self.agent.__class__.__name__ == 'DDPG':
-                            tf.contrib.summary.scalar('train/critic_loss', self.agent.critic_loss)
-                            tf.contrib.summary.scalar('train/actor_loss', self.agent.actor_loss)
-                        elif self.agent.__class__.__name__ == 'TD3':
-                            tf.contrib.summary.scalar('train/critic_loss1', self.agent.critic_loss1)
-                            tf.contrib.summary.scalar('train/critic_loss2', self.agent.critic_loss2)
-                            tf.contrib.summary.scalar('train/actor_loss', self.agent.actor_loss)
-                        else:
-                            tf.contrib.summary.scalar('train/loss', self.agent.loss)
-                        tf.contrib.summary.scalar('train/e_greedy', self.agent.epsilon)
-
-                        if (indexes != None):
-                            for i, td_error in enumerate(self.agent.td_error):
-                                self.replay_buf.update(indexes[i], td_error)
-
-                    if done or step == self.max_steps:
-                        total_steps += step
-                        tf.contrib.summary.scalar('train/total_steps', total_steps)
-                        tf.contrib.summary.scalar('train/steps_per_episode', step)
-                        tf.contrib.summary.scalar('train/total_reward', total_reward)
-                        tf.contrib.summary.scalar('train/average_reward', total_reward / step)
-                        print("episode: %d total_steps: %d  steps/episode: %d  total_reward: %0.2f"%(episode, total_steps, step, total_reward))
-                        metrics = OrderedDict({
-                            "episode": episode,
-                            "total_steps": total_steps,
-                            "steps/episode":step,
-                            "total_reward": total_reward})
-                        self.util.write_log(message=metrics)
-                        self.util.save_model()
-                        self.state_deque.clear()
-                        self.action_deque.clear()
-                        self.reward_deque.clear()
-                        break
-
-                    state = state_
-            # test
-            if episode % self.test_interval == 0 and learning_flag:
-                self.test(episode)
-
+    def episode_end(self):
         self.env.close()
+        return
 
+    def summary(self):
+        """
+        tensorboardに表示するための関数
+        """
+        if len(self.agent.bs.shape) == 4:
+            image = tf.expand_dims(self.agent.bs[:3,:,:,0],3)
+            tf.contrib.summary.image('train/input_img', tf.cast(image * 255.0, tf.uint8))
+        if self.agent.__class__.__name__ == 'DDPG':
+            tf.contrib.summary.scalar('train/critic_loss', self.agent.critic_loss)
+            tf.contrib.summary.scalar('train/actor_loss', self.agent.actor_loss)
+        elif self.agent.__class__.__name__ == 'TD3':
+            tf.contrib.summary.scalar('train/critic_loss1', self.agent.critic_loss1)
+            tf.contrib.summary.scalar('train/critic_loss2', self.agent.critic_loss2)
+            tf.contrib.summary.scalar('train/actor_loss', self.agent.actor_loss)
+        else:
+            tf.contrib.summary.scalar('train/loss', self.agent.loss)
+        tf.contrib.summary.scalar('train/e_greedy', self.agent.epsilon)
+        return
+    
+    def multi_step_reward(self, rewards, discount):
+        ret = 0.0
+        for idx, reward in enumerate(rewards):
+            ret += reward * (discount ** idx)
+        return ret, idx + 1
+
+
+class Trainer(BasedTrainer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    def step(self, episode):
+        with tf.contrib.summary.always_record_summaries():
+            state = self.env.reset()
+            total_reward = 0
+            for step in range(1, self.max_steps+1):
+                if self.render:
+                    self.env.render()
+
+                action = self.agent.choose_action(state)
+                state_, reward, done, _ = self.env.step(action)
+
+                # Multi-step learning
+                self.state_deque.append(state)
+                self.reward_deque.append(reward)
+                self.action_deque.append(action)
+
+                # the smaller theta and closer to center the better
+                if self.env.__class__.__name__ == 'CartPoleEnv':
+                    x, x_dot, theta, theta_dot = state_
+                    r1 = (self.env.x_threshold - abs(x))/self.env.x_threshold - 0.8
+                    r2 = (self.env.theta_threshold_radians - abs(theta))/self.env.theta_threshold_radians - 0.5
+                    reward = r1 + r2
+
+                if len(self.state_deque) == self.multi_step or done:
+                    t_reward, p_index = self.multi_step_reward(self.reward_deque, self.agent.discount)
+                    state = self.state_deque[0]
+                    action = self.action_deque[0]
+                    self.replay_buf.push(state, action, done, state_, t_reward, p_index)
+
+                total_reward += reward
+                if len(self.replay_buf) > self.replay_size and len(self.replay_buf) > self.n_warmup:
+                    indexes, transitions, weights = self.replay_buf.sample(self.agent.batch_size, episode/self.n_episode)
+                    train_data = map(np.array, zip(*transitions))
+                    self.agent.update_q_net(train_data, weights)
+                    self.learning_flag = 1
+                    self.summary()
+
+                    if (indexes != None):
+                        for i, td_error in enumerate(self.agent.td_error):
+                            self.replay_buf.update(indexes[i], td_error)
+
+                if done or step == self.max_steps:
+                    self.step_end(episode, step, total_reward)
+                    break
+
+                state = state_
+
+        # test
+        if episode % self.test_interval == 0 and self.learning_flag:
+            self.test(episode)
 
     def test(self, episode):
         """
@@ -180,10 +216,3 @@ class Trainer():
                 display_frames_as_gif(frames, "test_{}_{}".format(episode, test_episode), self.util.res_dir)
         print('-----------------------------------------------------------------------------')
         return
-
-
-    def multi_step_reward(self, rewards, discount):
-        ret = 0.0
-        for idx, reward in enumerate(rewards):
-            ret += reward * (discount ** idx)
-        return ret, idx + 1
