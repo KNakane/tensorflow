56324ce2f8feeebde66dce818afb5636866ef8b2
KNakane <koki.bsb@gmail.com>
2019-04-14 19:03:57 +0900

    [update] policy gradient

--- rl/agents/policy_gradient.py
+++ rl/agents/policy_gradient.py
@@ -12,16 +12,14 @@ class PolicyGradient(Agent):
         self.on_policy = True
     
     def _build_net(self):
-        if "tf.nn.softmax" not in self.model[-1]:
-            self.model[-1][2] = tf.nn.softmax
         # ------------------ build eval_net ------------------
         self.q_eval = eval(self.network)(model=self.model, out_dim=self.n_actions, name='Q_net', opt=self._optimizer, lr=self.lr, trainable=True, is_categorical=self.is_categorical)
 
     def inference(self, state):
-        return self.q_eval.inference(state)
+        return tf.nn.softmax(self.q_eval.inference(state))
 
-    def update_q_net(self, replay_data):
-        self.bs, ba, done, bs_, br = replay_data
+    def update_q_net(self, replay_data, weights):
+        self.bs, ba, done, bs_, br, p_idx = replay_data
         eval_act_index = ba
         reward = br
         done = done
@@ -34,7 +32,7 @@ class PolicyGradient(Agent):
             selected_action_probs = tf.reduce_mean(one_hot_actions * q_eval, axis=1)
             clipped = tf.clip_by_value(selected_action_probs, 1e-10, 1.0)
             self.td_error = -tf.log(clipped) * reward
-            self.loss = tf.reduce_mean(self.td_error)
+            self.loss = tf.reduce_mean(self.td_error * weights)
         self.q_eval.optimize(self.loss, global_step, tape)
 
         # increasing epsilon
