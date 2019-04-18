80dbb69d305a313ac8e78220a76308a06b81e654
Koki Nakane <k_nakane@mmini02.isl.melco.co.jp>
2019-04-18 20:47:37 +0900

    [update] setting matplotlib

--- utility/event_getter.py
+++ utility/event_getter.py
@@ -10,6 +10,15 @@ from itertools import chain
 import matplotlib.ticker as ticker
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 
+plt.rcParams['font.family'] ='sans-serif'#使用するフォント
+plt.rcParams['xtick.direction'] = 'in'#x軸の目盛線が内向き('in')か外向き('out')か双方向か('inout')
+plt.rcParams['ytick.direction'] = 'in'#y軸の目盛線が内向き('in')か外向き('out')か双方向か('inout')
+plt.rcParams['xtick.major.width'] = 1.0#x軸主目盛り線の線幅
+plt.rcParams['ytick.major.width'] = 1.0#y軸主目盛り線の線幅
+plt.rcParams['font.size'] = 8 #フォントの大きさ
+plt.rcParams['axes.linewidth'] = 1.0# 軸の線幅edge linewidth。囲みの太さ
+
+
 class EventGetter():
     """ tensorboardに表示したデータを取得し、グラフを作成する """
     def __init__(self, events_list, prob):
