7ea209ecb51b020681a9695b491bc23f545601b6
KNakane <koki.bsb@gmail.com>
2019-04-20 22:11:25 +0900

    [fix] gather a2c and a3c to actor_critic

--- rl/agents/actor_critic.py
+++ rl/agents/actor_critic.py
@@ -4,7 +4,29 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../../network'))
 import numpy as np
 import tensorflow as tf
 from agent import Agent
-from eager_nn import A2CNet
+from eager_nn import A2CNet, A3CNet
+
+
+class A3C(Agent):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def _build_net(self):
+        # ------------------ build eval_net ------------------
+        with tf.variable_scope('eval_net'):
+            self.q_eval = eval(self.network)(model=self.model, out_dim=self.n_actions, name='Q_net', opt=self._optimizer, lr=self.lr, trainable=True, is_categorical=self.is_categorical, is_noise=self.is_noise)
+
+        # ------------------ build target_net ------------------
+        with tf.variable_scope('target_net'):
+            self.q_next = eval(self.network)(model=self.model, out_dim=self.n_actions, name='target_net', trainable=False, is_categorical=self.is_categorical, is_noise=self.is_noise)
+    
+    def inference(self, state):
+        if self.is_categorical:
+            return tf.argmax(tf.reduce_sum(self.q_eval.inference(state) * self.z_list_broadcasted, axis=2), axis=1)
+        else:
+            return self.q_eval.inference(state)
+
+
 
 class A2C(Agent):
     def __init__(self, *args, **kwargs):
