a280128086ac4bb5ac1243c3c54c1eb3c8bd0446
Koki Nakane <k_nakane@mbp02.isl.melco.co.jp>
2019-07-05 13:30:00 +0900

    [update] gan evaluation

--- GAN/eval.py
+++ GAN/eval.py
@@ -6,7 +6,6 @@ import tensorflow as tf
 from load import Load
 from utils import Utils
 from gan import GAN, DCGAN, WGAN, WGAN_GP, LSGAN, ACGAN, DRAGAN
-from train import set_model
 
 
 def main(args):
@@ -18,39 +17,24 @@ def main(args):
 
     # load dataset
     data = Load(FLAGS.data)
-    batch_size = 3000
-    iteration = data.x_test.shape[0] // batch_size
-    test = data.load_test(data.x_test, data.y_test, batch_size)
-    test_iter = test.make_initializable_iterator()
-    inputs, labels = test_iter.get_next()
+    batch_size = 100
+    inputs = tf.random.uniform([batch_size*3, FLAGS.z_dim],-1,1)
 
-    model_set = set_model(data.output_dim)
     model = eval(FLAGS.network)(z_dim=FLAGS.z_dim,
                                 size=data.size,
                                 channel=data.channel,
                                 lr=0.0,
                                 class_num=data.output_dim,
                                 conditional=FLAGS.conditional,
-                                opt=FLAGS.opt,
+                                opt=None,
                                 trainable=False)
-
-    logits = model.inference(inputs)
-    logits  = tf.identity(logits, name="output_logits")
-    correct_prediction = tf.equal(tf.argmax(logits,1), tf.argmax(labels, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
     tf.train.Saver()
     with tf.Session() as sess:
-        sess.run([test_iter.initializer],feed_dict={data.test_placeholder: data.x_test,
-                                        data.test_labels_placeholder: data.y_test})
         utils = Utils(sess=sess)
         if utils.restore_model(FLAGS.ckpt_dir):
-            avg_accuracy = 0
-            for i in range(iteration):
-                test_accuracy = sess.run(accuracy)
-                avg_accuracy += test_accuracy
-                print("accuracy_{} : {}".format(i, test_accuracy))
-            print("average_accuracy : {}".format(avg_accuracy / iteration))
+            image = sess.run(model.predict(inputs, batch_size*3))
+            utils.gan_plot(image)
             return
         else:
             return
@@ -58,6 +42,7 @@ def main(args):
 if __name__ == '__main__':
     flags = tf.app.flags
     FLAGS = flags.FLAGS
+    flags.DEFINE_integer('z_dim', '100', 'Latent z dimension')
     flags.DEFINE_string('network', 'GAN', 'Choice the training data name -> [GAN, DCGAN, WGAN, WGAN_GP, LSGAN, ACGAN, DRAGAN]')
     flags.DEFINE_string('ckpt_dir', './results/181225_193106/model', 'Choice the checkpoint directpry')
     flags.DEFINE_string('data', 'mnist', 'Choice the training data name -> ["mnist","cifar10","cifar100","kuzushiji"]')
