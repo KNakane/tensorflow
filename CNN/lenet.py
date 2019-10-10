1b4210ef316963e3d0ef4f0e16901e64379f8708
Koki Nakane <k_nakane@mmini02.isl.melco.co.jp>
2019-10-10 18:03:42 +0900

    [update] VGG

--- CNN/lenet.py
+++ CNN/lenet.py
@@ -8,13 +8,13 @@ class LeNet(MyModel):
         super().__init__(*args, **kwargs)
 
     def _build(self):
-        self.conv1 = tf.keras.layers.Conv2D(6, kernel_size=(5, 5), padding='valid', activation='relu')
+        self.conv1 = tf.keras.layers.Conv2D(6, kernel_size=(5, 5), padding='valid', activation='relu', kernel_regularizer=self.l2_regularizer)
         self.pooling1 = tf.keras.layers.MaxPooling2D(padding='same')
-        self.conv2 = tf.keras.layers.Conv2D(16, kernel_size=(5, 5), padding='valid', activation='relu')
+        self.conv2 = tf.keras.layers.Conv2D(16, kernel_size=(5, 5), padding='valid', activation='relu', kernel_regularizer=self.l2_regularizer)
         self.pooling2 = tf.keras.layers.MaxPooling2D(padding='same')
         self.flat = tf.keras.layers.Flatten()
-        self.fc1 = tf.keras.layers.Dense(120, activation='relu')
-        self.fc2 = tf.keras.layers.Dense(84, activation='relu')
+        self.fc1 = tf.keras.layers.Dense(120, activation='relu', kernel_regularizer=self.l2_regularizer)
+        self.fc2 = tf.keras.layers.Dense(84, activation='relu', kernel_regularizer=self.l2_regularizer)
         self.out = tf.keras.layers.Dense(self.out_dim, activation='softmax')
         return
     
@@ -23,6 +23,43 @@ class LeNet(MyModel):
         x = self.pooling1(x)
         x = self.conv2(x)
         x = self.pooling2(x)
+        x = self.flat(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.out(x)
+        return x
+
+
+class VGG(MyModel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def _build(self):
+        self.conv1 = tf.keras.layers.Conv2D(64, kernel_size=(3, 3), padding='same', activation='relu', kernel_regularizer=self.l2_regularizer)
+        self.conv2 = tf.keras.layers.Conv2D(64, kernel_size=(3, 3), padding='same', activation='relu', kernel_regularizer=self.l2_regularizer)
+        self.pooling1 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2), padding='same')
+        self.conv3 = tf.keras.layers.Conv2D(128, kernel_size=(3, 3), padding='same', activation='relu', kernel_regularizer=self.l2_regularizer)
+        self.conv4 = tf.keras.layers.Conv2D(128, kernel_size=(3, 3), padding='same', activation='relu', kernel_regularizer=self.l2_regularizer)
+        self.pooling2 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2), padding='same')
+        self.conv5 = tf.keras.layers.Conv2D(256, kernel_size=(3, 3), padding='same', activation='relu', kernel_regularizer=self.l2_regularizer)
+        self.conv6 = tf.keras.layers.Conv2D(256, kernel_size=(3, 3), padding='same', activation='relu', kernel_regularizer=self.l2_regularizer)
+        self.pooling3 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2), padding='same')
+        self.flat = tf.keras.layers.Flatten()
+        self.fc1 = tf.keras.layers.Dense(500, activation='relu', kernel_regularizer=self.l2_regularizer)
+        self.fc2 = tf.keras.layers.Dense(200, activation='relu', kernel_regularizer=self.l2_regularizer)
+        self.out = tf.keras.layers.Dense(self.out_dim, activation='softmax')
+        return
+    
+    def inference(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.pooling1(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.pooling2(x)
+        x = self.conv5(x)
+        x = self.conv6(x)
+        x = self.pooling3(x)
         x = self.flat(x)
         x = self.fc1(x)
         x = self.fc2(x)
