888380a3dbedabf3ae647d8467e79cc85d2102fd
Koki Nakane <k_nakane@mmini02.isl.melco.co.jp>
2019-10-10 14:03:57 +0900

    [update] show dataset image in tensorboard

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -35,31 +35,35 @@ class Trainer():
         return tf.summary.create_file_writer(self.util.tf_board)
 
     @tf.function
-    def _train_body(self, model, images, labels):
+    def _train_body(self, images, labels):
         with tf.GradientTape() as tape:
-            y_pre = model.inference(images)
-            loss = model.loss(y_pre, labels)
-        model.optimize(loss, tape)
-        acc = model.accuracy(y_pre, labels)
+            y_pre = self.model.inference(images)
+            loss = self.model.loss(y_pre, labels)
+        self.model.optimize(loss, tape)
+        acc = self.model.accuracy(y_pre, labels)
         return loss, acc
 
     @tf.function
-    def _test_body(self, model, images, labels):
-        y_pre = model.inference(images)
-        loss = model.loss(y_pre, labels)
-        acc = model.accuracy(y_pre, labels)
+    def _test_body(self, images, labels):
+        y_pre = self.model.inference(images)
+        loss = self.model.loss(y_pre, labels)
+        acc = self.model.accuracy(y_pre, labels)
         return loss, acc
 
     def epoch_end(self, metrics, other=None):
         tf.summary.experimental.set_step(metrics['epoch'])
-        tf.summary.scalar('epoch', metrics['epoch'])
+        tf.summary.scalar('detail/epoch', metrics['epoch'])
+        tf.summary.scalar('detail/time_per_step', metrics['time/step'])
+        tf.summary.scalar('detail/learning_rate', self.model.optimizer.lr)
         tf.summary.scalar('train/loss', metrics['train_loss'])
         tf.summary.scalar('train/accuracy', metrics['train_accuracy'])
         tf.summary.scalar('test/loss', metrics['test_loss'])
         tf.summary.scalar('test/accuracy', metrics['test_accuracy'])
-        tf.summary.scalar('time_per_step', metrics['time/step'])
+        if 'train_image' in other and len(other['train_image'].shape) == 4:
+            tf.summary.image('train/image', other['train_image'])
+            tf.summary.image('test/image', other['test_image'])
 
-        
+
         print("epoch: %d  train_loss: %.4f  train_accuracy: %.3f test_loss: %.4f  test_accuracy: %.3f  time/step: %0.3fms" 
                                 %(metrics['epoch'], metrics['train_loss'], metrics['train_accuracy'], metrics['test_loss'], metrics['test_accuracy'], metrics['time/step']))
         self.util.write_log(message=metrics)
@@ -82,19 +86,28 @@ class Trainer():
 
         for i in range(1, self.n_epoch+1):
             start_time = time.time()
-            for (_, (images, labels)) in enumerate(train_dataset.take(self.batch_size)):
-                loss, train_accuracy = self._train_body(self.model, images, labels)
+            for (_, (train_images, train_labels)) in enumerate(train_dataset.take(self.batch_size)):
+                loss, train_accuracy = self._train_body(train_images, train_labels)
                 train_loss = train_loss_fn(loss)
             time_per_episode = time.time() - start_time
-            for (_, (images, labels)) in enumerate(test_dataset.take(self.batch_size)):
-                loss, test_accuracy = self._test_body(self.model, images, labels)
+            for (_, (test_images, test_labels)) in enumerate(test_dataset.take(self.batch_size)):
+                loss, test_accuracy = self._test_body(test_images, test_labels)
                 test_loss = test_loss_fn(loss)
+
+            # Training results
             metrics = OrderedDict({
-            "epoch": i,
-            "train_loss": train_loss.numpy(),
-            "train_accuracy":train_accuracy.numpy(),
-            "test_loss": test_loss.numpy(),
-            "test_accuracy" : test_accuracy.numpy(),
-            "time/step": time_per_episode})
-            self.epoch_end(metrics)
+                "epoch": i,
+                "train_loss": train_loss.numpy(),
+                "train_accuracy":train_accuracy.numpy(),
+                "test_loss": test_loss.numpy(),
+                "test_accuracy" : test_accuracy.numpy(),
+                "time/step": time_per_episode
+            })
+
+            #
+            other_metrics = OrderedDict({
+                "train_image" : train_images[:3],
+                "test_image" : test_images[:3]
+            })
+            self.epoch_end(metrics, other_metrics)
         return
