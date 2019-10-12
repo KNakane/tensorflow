c98ec1af36533f089e226fb9b15e0a5cfea418c3
KNakane <koki.bsb@gmail.com>
2019-10-12 14:09:55 +0900

    [add] eval

---
+++ eval.py
@@ -0,0 +1,12 @@
+import os, sys
+from absl import app
+from absl import flags
+import tensorflow as tf
+
+
+def main(args):
+    return
+
+if __name__ == '__main__':
+    FLAGS = flags.FLAGS
+    app.run(main)
