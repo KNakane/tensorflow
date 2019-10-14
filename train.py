407d734bfa29cd529d3d16ca33342b2b2948a09b
KNakane <koki.bsb@gmail.com>
2019-10-14 19:16:21 +0900

    [add] GAN

--- train.py
+++ train.py
@@ -4,8 +4,10 @@ from absl import flags
 import tensorflow as tf
 from CNN.lenet import LeNet, VGG
 from AutoEncoder.model import AutoEncoder, VAE
+from GAN.gan import GAN
+from GAN.dcgan import DCGAN
 from dataset.load import Load
-from trainer.trainer import Trainer, AE_Trainer
+from trainer.trainer import Trainer, AE_Trainer, GAN_Trainer
 from collections import OrderedDict
 
 
@@ -48,18 +50,48 @@ def construction_image(FLAGS):
     trainer.train()
     return
 
+def GAN_fn(FLAGS):
+    message = OrderedDict({
+        "Network": FLAGS.network,
+        "Conditional": FLAGS.conditional,
+        "data": FLAGS.data,
+        "z_dim": FLAGS.z_dim,
+        "epoch":FLAGS.n_epoch,
+        "batch_size": FLAGS.batch_size,
+        "Optimizer":FLAGS.opt,
+        "learning_rate":FLAGS.lr,
+        "n_disc_update":FLAGS.n_disc_update,
+        "l2_norm":FLAGS.l2_norm,
+        "Augmentation": FLAGS.aug})
+
+    data = Load(FLAGS.data)
+    model = eval(FLAGS.network)(z_dim=FLAGS.z_dim,
+                                size=data.size,
+                                channel=data.channel,
+                                name=FLAGS.network,
+                                class_num=data.output_dim,
+                                lr=FLAGS.lr, opt=FLAGS.opt, l2_reg=FLAGS.l2_norm)
+
+    #training
+    trainer = GAN_Trainer(FLAGS, message, data, model, FLAGS.network)
+    trainer.train()
+    return
+
+
 def main(args):
     if FLAGS.network == 'LeNet' or FLAGS.network == 'VGG':
         image_recognition(FLAGS)
     elif FLAGS.network == 'AutoEncoder' or FLAGS.network == 'VAE':
         construction_image(FLAGS)
+    elif FLAGS.network == 'GAN' or FLAGS.network == 'DCGAN':
+        GAN_fn(FLAGS)
     else:
         raise NotImplementedError()
     return
 
 if __name__ == '__main__':
     FLAGS = flags.FLAGS
-    flags.DEFINE_string('network', 'LeNet', 'Choice the training data name -> [LeNet,VGG,AutoEncoder,VAE]')
+    flags.DEFINE_string('network', 'LeNet', 'Choice the training data name -> [LeNet,VGG,AutoEncoder,VAE,GAN,DCGAN]')
     flags.DEFINE_string('data', 'mnist', 'Choice the training data name -> ["mnist","cifar10","cifar100","kuzushiji"]')
     flags.DEFINE_integer('n_epoch', '1000', 'Input max epoch')
     flags.DEFINE_integer('batch_size', '32', 'Input batch size')
@@ -68,6 +100,9 @@ if __name__ == '__main__':
     flags.DEFINE_string('aug','None','Choice the Augmentation -> ["shift","mirror","rotate","shift_rotate","cutout","random_erace"]')
     flags.DEFINE_bool('denoise', 'False', 'True : Denoising AE, False : standard AE')
     flags.DEFINE_bool('l2_norm', 'False', 'Input learning rate')
+    flags.DEFINE_integer('z_dim', '100', 'Latent z dimension')
+    flags.DEFINE_bool('conditional', 'False', 'Conditional true or false')
+    flags.DEFINE_integer('n_disc_update', '2', 'Input max epoch')
     flags.DEFINE_string('init_model', 'None', 'Choice the checkpoint directpry(ex. ./results/181225_193106/model)')
     flags.DEFINE_integer('checkpoints_to_keep', 5,'checkpoint keep count')
     flags.DEFINE_integer('keep_checkpoint_every_n_hours', 1, 'checkpoint create ')
--- trainer/trainer.py
+++ trainer/trainer.py
@@ -48,7 +48,7 @@ class Trainer():
 
     @tf.function
     def _test_body(self, images, labels):
-        y_pre = self.model.inference(images)
+        y_pre = self.model.test_inference(images)
         loss = self.model.loss(y_pre, labels)
         acc = self.model.accuracy(y_pre, labels)
         return y_pre, loss, acc
@@ -57,7 +57,7 @@ class Trainer():
         learning_rate = self.model.optimizer.lr(metrics['epoch']).numpy() if type(self.model.optimizer.lr) is tf.optimizers.schedules.ExponentialDecay else self.model.optimizer.lr
         tf.summary.experimental.set_step(metrics['epoch'])
         tf.summary.scalar('detail/epoch', metrics['epoch'])
-        tf.summary.scalar('detail/time_per_step', metrics['time/step'])
+        tf.summary.scalar('detail/time_per_step', metrics['time/epoch'])
         tf.summary.scalar('detail/learning_rate', learning_rate)
         tf.summary.scalar('train/loss', metrics['train_loss'])
         tf.summary.scalar('train/accuracy', metrics['train_accuracy'])
@@ -71,8 +71,8 @@ class Trainer():
             tf.summary.image('test/decode_image', other['Decode_test_image'])
 
 
-        print("epoch: %d  train_loss: %.4f  train_accuracy: %.3f test_loss: %.4f  test_accuracy: %.3f  time/step: %0.3fms" 
-                                %(metrics['epoch'], metrics['train_loss'], metrics['train_accuracy'], metrics['test_loss'], metrics['test_accuracy'], metrics['time/step']))
+        print("epoch: %d  train_loss: %.4f  train_accuracy: %.3f test_loss: %.4f  test_accuracy: %.3f  time/epoch: %0.3fms" 
+                                %(metrics['epoch'], metrics['train_loss'], metrics['train_accuracy'], metrics['test_loss'], metrics['test_accuracy'], metrics['time/epoch']))
         self.util.write_log(message=metrics)
         if metrics['epoch'] % self.save_checkpoint_steps == 0:
             self.util.save_model(global_step=metrics['epoch'])
@@ -107,7 +107,7 @@ class Trainer():
                 "train_accuracy":train_accuracy.numpy(),
                 "test_loss": test_loss.numpy(),
                 "test_accuracy" : test_accuracy.numpy(),
-                "time/step": time_per_episode
+                "time/epoch": time_per_episode
             })
 
             #
@@ -149,13 +149,102 @@ class AE_Trainer(Trainer):
                 "train_accuracy":train_accuracy,
                 "test_loss": test_loss.numpy(),
                 "test_accuracy" : test_accuracy.numpy(),
-                "time/step": time_per_episode
+                "time/epoch": time_per_episode
             })
 
             #
             other_metrics = OrderedDict({
                 "train_image" : train_images[:3],
                 "test_image" : test_images[:3],
+                "Decode_train_image" : train_pre,
+                "Decode_test_image" : test_pre
+            })
+            self.epoch_end(metrics, other_metrics)
+        return
+
+
+class GAN_Trainer(Trainer):
+    def __init__(self,
+                 FLAGS,
+                 message,
+                 data,
+                 model,
+                 name):
+        super().__init__(FLAGS, message, data, model, name)
+        self.z_dim = FLAGS.z_dim
+        self.n_disc_update = FLAGS.n_disc_update
+        self.condtional = FLAGS.conditional
+
+    @tf.function
+    def _train_body(self, images, labels):
+        with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+            fake_logit, real_logit, fake_image = self.model.inference(images, self.batch_size)
+            d_loss, g_loss = self.model.loss(fake_logit, real_logit)
+        self.model.discriminator_optimize(d_loss, disc_tape, self.n_disc_update)
+        self.model.generator_optimize(g_loss, gen_tape)
+        acc = self.model.accuracy(real_logit, fake_logit)
+        return fake_image, d_loss, g_loss, acc
+
+    @tf.function
+    def _test_body(self, images):
+        fake_logit = self.model.test_inference(images, self.batch_size*3)
+        return fake_logit
+
+    def epoch_end(self, metrics, other=None):
+        Discriminator_learning_rate = self.model.d_optimizer.lr(metrics['epoch']).numpy() if type(self.model.d_optimizer.lr) is tf.optimizers.schedules.ExponentialDecay else self.model.d_optimizer.lr
+        Generator_learning_rate = self.model.g_optimizer.lr(metrics['epoch']).numpy() if type(self.model.g_optimizer.lr) is tf.optimizers.schedules.ExponentialDecay else self.model.g_optimizer.lr
+        tf.summary.experimental.set_step(metrics['epoch'])
+        tf.summary.scalar('detail/epoch', metrics['epoch'])
+        tf.summary.scalar('detail/time_per_step', metrics['time/epoch'])
+        tf.summary.scalar('detail/Discriminator_learning_rate', Discriminator_learning_rate)
+        tf.summary.scalar('detail/Generator_learning_rate', Generator_learning_rate)
+        tf.summary.scalar('train/Discriminator_loss', metrics['Discriminator_loss'])
+        tf.summary.scalar('train/Generator_loss', metrics['Generator_loss'])
+        if 'train_image' in other and len(other['train_image'].shape) == 4:
+            tf.summary.image('train/image', other['train_image'])
+        if 'Decode_train_image' in other:
+            tf.summary.image('train/decode_image', other['Decode_train_image'])
+            tf.summary.image('test/decode_image', other['Decode_test_image'])
+        
+        print("epoch: %d  Discriminator_loss: %.4f  Generator_loss: %.4f  train_accuracy: %.3f time/epoch: %0.3fms" 
+                                %(metrics['epoch'], metrics['Discriminator_loss'], metrics['Generator_loss'], metrics['train_accuracy'], metrics['time/epoch']))
+        self.util.write_log(message=metrics)
+        if metrics['epoch'] % self.save_checkpoint_steps == 0:
+            self.util.save_model(global_step=metrics['epoch'])
+        return
+
+    def train(self):
+        board_writer = self.begin_train()
+        board_writer.set_as_default()
+
+        if self.restore_dir is not None:
+            self.util.restore_agent(self.model ,self.restore_dir)
+        
+        train_dataset, _ = self.load()
+        test_inputs = tf.random.uniform([self.batch_size*3, self.z_dim],-1,+1)
+
+        for i in range(1, self.n_epoch+1):
+            start_time = time.time()
+            for (j, (train_images, _)) in enumerate(train_dataset.take(self.batch_size)):
+                train_pre, dis_loss, gene_loss, train_accuracy = self._train_body(train_images, train_images)
+            time_per_episode = time.time() - start_time
+            test_pre = self._test_body(test_inputs)
+
+            if i == 1 or i % 5 == 0:
+                self.util.plot_figure(test_pre.numpy(), i)
+
+            # Training results
+            metrics = OrderedDict({
+                "epoch": i,
+                "Discriminator_loss": dis_loss.numpy(),
+                "Generator_loss": gene_loss.numpy(),
+                "train_accuracy":train_accuracy,
+                "time/epoch": time_per_episode
+            })
+
+            #
+            other_metrics = OrderedDict({
+                "train_image" : train_images[:3],
                 "Decode_train_image" : train_pre,
                 "Decode_test_image" : test_pre
             })
