a493cc5278bc076e634747c6eaa01d577576c457
KNakane <koki.bsb@gmail.com>
2020-01-05 14:55:44 +0900

    [add] resnet in CNN

--- CNN/model.py
+++ CNN/model.py
@@ -44,3 +44,48 @@ class MyModel(Model):
     def accuracy(self, logits, answer):
         self.accuracy_function(y_true=answer, y_pred=logits)
         return self.accuracy_function.result()
+
+
+class BasedResNet(MyModel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._first_layer()
+
+    def _first_layer(self):
+        self.conv1 = tf.keras.layers.Convolution2D(64, (7,7), (2,2), 'same', kernel_regularizer=self.l2_regularizer)
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.relu1 = tf.keras.layers.ReLU()
+        self.pool1 = tf.keras.layers.MaxPool2D(pool_size=(3,3), strides=(2, 2), padding='same')
+        return
+    
+    def _building_block(self, channel_out=64, downsample=False):
+        return ResidualBlock(channel_out=channel_out, downsample=downsample, l2_reg_scale=self.l2_regularizer)
+
+
+class ResidualBlock(Model):
+    def __init__(self, channel_out=64, downsample=False, l2_reg_scale=None):
+        super().__init__()
+        self.downsample = downsample
+        if self.downsample:
+            self.conv1 = tf.keras.layers.Convolution2D(channel_out, (3,3), (2,2), kernel_regularizer=l2_reg_scale)
+            self.downsampling = tf.keras.layers.Convolution2D(channel_out, (1,1), (2,2))
+        else:
+            self.conv1 = tf.keras.layers.Convolution2D(channel_out, (3,3), padding='same', kernel_regularizer=l2_reg_scale)
+            self.downsampling = tf.keras.layers.Convolution2D(channel_out, (1,1))
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.relu1 = tf.keras.layers.ReLU()
+        self.conv2 = tf.keras.layers.Convolution2D(channel_out, (3,3), padding='same', kernel_regularizer=l2_reg_scale)
+        self.bn2 = tf.keras.layers.BatchNormalization()
+        self.add = tf.keras.layers.Add()
+        self.relu2 = tf.keras.layers.ReLU()
+
+    def call(self, x, training=True):
+        x = self.downsampling(x)
+        h = self.conv1(x, training=training)
+        h = self.bn1(h, training=training)
+        h = self.relu1(h, training=training)
+        h = self.conv2(h, training=training)
+        h = self.bn2(h, training=training)
+        h = self.add([x, h])
+        h = self.relu2(h)
+        return h
