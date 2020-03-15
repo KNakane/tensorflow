902d0f5d07422318f029d3b3ba6cb31cb042c6f0
KNakane <koki.bsb@gmail.com>
2020-03-15 13:40:22 +0900

    [add] wgan and wgan gp

---
+++ GAN/wgan.py
@@ -0,0 +1,65 @@
+import os, sys
+import numpy as np
+import tensorflow as tf
+from GAN.dcgan import Discriminator, Generator, DCGAN
+
+class WGAN(DCGAN):
+    """
+    Wasserstein GAN
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def weight_clipping(self):
+        with tf.name_scope('weight_clipping'):
+            for p in self.D.weights:
+                p.assign(tf.clip_by_value(p, -0.01, 0.01))
+        return
+
+    def loss(self, fake_logit, real_logit):
+        with tf.name_scope('loss'):
+            with tf.name_scope('Discriminator_loss'):
+                d_loss = -tf.reduce_mean(real_logit + self.eps) + tf.reduce_mean(fake_logit + self.eps)
+            with tf.name_scope('Generator_loss'):
+                g_loss = -tf.reduce_mean(fake_logit + self.eps)
+            return d_loss, g_loss
+
+    def discriminator_optimize(self, d_loss, tape=None, n_update=1):
+        assert tape is not None, 'please set tape in optimize'
+        d_grads = tape.gradient(d_loss, self.D.trainable_variables)
+        for _ in range(n_update):
+            self.d_optimizer.method.apply_gradients(zip(d_grads, self.D.trainable_variables))
+            self.weight_clipping()
+        return
+
+
+class WGANGP(WGAN):
+    """
+    Wesserstein GAN + Gradient penalty
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.grad_penalty_weight = tf.constant(10., dtype=tf.float32)
+
+    def inference(self, inputs, batch_size, labels=None, trainable=True):
+        fake_logit, real_logit, fake_img = super().inference(inputs, batch_size, labels, trainable)
+        e = tf.random.uniform([batch_size, 1, 1, 1], 0, 1)
+        x_hat = e * inputs + (1 - e) * fake_img
+        x_hat_logit = self.D(x_hat, trainable=trainable)
+        grad = tf.gradients(x_hat_logit, [x_hat])[0]
+        return [fake_logit, grad], real_logit, fake_img
+
+    def loss(self, fake_logits, real_logit):
+        fake_logit, grad = fake_logits[0], fake_logits[1]
+        with tf.name_scope('loss'):
+            with tf.name_scope('Discriminator_loss'):
+                d_loss = tf.reduce_mean(fake_logit - real_logit)
+                gp = tf.reduce_mean(tf.square(tf.sqrt(tf.reduce_sum(tf.square(grad), axis=[1, 2, 3])) - 1))
+                d_loss += self.grad_penalty_weight * gp
+            with tf.name_scope('Generator_loss'):
+                g_loss = -tf.reduce_mean(fake_logit + self.eps)
+            return d_loss, g_loss
+
+    def accuracy(self, real_logit, fake_logits):
+        fake_logit = fake_logits[0]
+        return  (tf.reduce_mean(tf.cast(fake_logit < 0.5, tf.float32)) + tf.reduce_mean(tf.cast(real_logit > 0.5, tf.float32))) / 2.
