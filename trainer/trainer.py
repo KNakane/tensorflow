5f3bb73072365ffdfc586bbf22e8ee5b424a091a
KNakane <koki.bsb@gmail.com>
2020-03-15 13:41:57 +0900

    [update] create gan img in each epoch

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -29,8 +29,8 @@ class Trainer():
             self.aug = FLAGS.aug
 
     def load(self):
-        train_dataset = self.data.load(self.data.x_train, self.data.y_train, batch_size=self.batch_size, is_training=True, augmentation=self.aug)
-        test_dataset = self.data.load(self.data.x_test, self.data.y_test, batch_size=self.batch_size*3, is_training=False)
+        train_dataset = self.data.load(batch_size=self.batch_size, is_training=True, augmentation=self.aug)
+        test_dataset = self.data.load(batch_size=self.batch_size*3, is_training=False)
         return train_dataset, test_dataset
 
     def begin_train(self):
@@ -296,8 +296,8 @@ class GAN_Trainer(Trainer):
                     tf.summary.trace_export("summary", step=1, profiler_outdir=self.util.tf_board)
                     tf.summary.trace_off()
 
-                if i == 1 or i % 50 == 0:
-                    self.util.plot_figure(test_pre.numpy(), i)
+                
+                self.util.plot_figure(test_pre.numpy(), i)
 
                 # Training results
                 metrics = OrderedDict({
@@ -315,4 +315,74 @@ class GAN_Trainer(Trainer):
                     "Decode_test_image" : test_pre
                 })
                 self.epoch_end(metrics, other_metrics)
+        return
+
+class Seq_Trainer(Trainer):
+    def __init__(self,
+                 FLAGS,
+                 message,
+                 data,
+                 model,
+                 name):
+        super().__init__(FLAGS, message, data, model, name)
+
+    def train(self):
+        board_writer = self.begin_train()
+
+        if self.restore_dir is not None:
+            self.util.restore_agent(self.model ,self.restore_dir)
+        
+        train_dataset, test_dataset = self.load()
+
+        # set mean loss
+        train_loss_fn = tf.keras.metrics.Mean(name='train_loss')
+        test_loss_fn = tf.keras.metrics.Mean(name='test_loss')
+
+        # Graph for tensorboard
+        tf.summary.trace_on(graph=True, profiler=True)
+        with board_writer.as_default():
+            for i in range(1, self.n_epoch+1):
+                start_time = time.time()
+                for (train_images, train_labels) in train_dataset:
+                    _, loss, train_accuracy = self._train_body(train_images, train_labels)
+                    train_loss = train_loss_fn(loss)
+                if i == 1:
+                    tf.summary.trace_export("summary", step=1, profiler_outdir=self.util.tf_board)
+                    tf.summary.trace_off()
+
+                time_per_episode = time.time() - start_time
+                for (test_images, test_labels) in test_dataset:
+                    _, loss, test_accuracy = self._test_body(test_images, test_labels)
+                    test_loss = test_loss_fn(loss)
+
+                # Training results
+                metrics = OrderedDict({
+                    "epoch": i,
+                    "train_loss": train_loss.numpy(),
+                    "train_accuracy":train_accuracy.numpy(),
+                    "test_loss": test_loss.numpy(),
+                    "test_accuracy" : test_accuracy.numpy(),
+                    "time/epoch": time_per_episode
+                })
+
+                self.epoch_end(metrics, other_metrics)
+        
+        return
+
+    def epoch_end(self, metrics, other=None):
+        learning_rate = self.model.optimizer.lr(metrics['epoch']).numpy() if type(self.model.optimizer.lr) is tf.optimizers.schedules.ExponentialDecay else self.model.optimizer.lr
+        tf.summary.experimental.set_step(metrics['epoch'])
+        tf.summary.scalar('detail/epoch', metrics['epoch'])
+        tf.summary.scalar('detail/time_per_step', metrics['time/epoch'])
+        tf.summary.scalar('detail/learning_rate', learning_rate)
+        tf.summary.scalar('train/loss', metrics['train_loss'])
+        tf.summary.scalar('train/accuracy', metrics['train_accuracy'])
+        tf.summary.scalar('test/loss', metrics['test_loss'])
+        tf.summary.scalar('test/accuracy', metrics['test_accuracy'])
+
+        print("epoch: %d  train_loss: %.4f  train_accuracy: %.3f test_loss: %.4f  test_accuracy: %.3f  time/epoch: %0.3fs" 
+                                %(metrics['epoch'], metrics['train_loss'], metrics['train_accuracy'], metrics['test_loss'], metrics['test_accuracy'], metrics['time/epoch']))
+        self.util.write_log(message=metrics)
+        if metrics['epoch'] % self.save_checkpoint_steps == 0:
+            self.util.save_model(global_step=metrics['epoch'])
         return
