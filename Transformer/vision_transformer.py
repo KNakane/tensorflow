3ff6d46b2804c40aa1277f6333848cdb8a8d6d2d
KNakane <koki.bsb@gmail.com>
2020-11-03 09:08:27 +0900

    [update] implement ViT

--- Transformer/vision_transformer.py
+++ Transformer/vision_transformer.py
@@ -1,27 +1,59 @@
 import os, sys
 import numpy as np
 import tensorflow as tf
-from CNN.model import MyModel
+from tensorflow.keras.models import Model
 from optimizer.optimizer import *
+from network.activation_func import gelu
 
 
 # https://github.com/emla2805/vision-transformer/blob/master/model.py
 
-class VisionTransformer(MyModel):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class VisionTransformer(Model):
+    def __init__(self, 
+                 model=None,
+                 name='Model',
+                 input_shape=None,
+                 out_dim=10,
+                 opt="Adam",   # Choice the optimizer -> ["SGD","Momentum","Adadelta","Adagrad","Adam","RMSProp"]
+                 lr=0.001,
+                 l2_reg=False,
+                 l2_reg_scale=0.0001,
+                 patch_size=4,
+                 d_model=64,
+                 num_layers=4,
+                 num_heads=4,
+                 mlp_dim=128,
+                 dropout=0.1
+                 ):
+        super().__init__()
+        self.model_name = name
+        self.out_dim = out_dim
+        self.optimizer = eval(opt)(learning_rate=lr, decay_step=None, decay_rate=0.95)
+        self.l2_regularizer = tf.keras.regularizers.l2(l2_reg_scale) if l2_reg else None
 
-        num_patches = (self.input_shape[0] // patch_size) ** 2
-        self.patch_dim = channels * patch_size ** 2
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.mlp_dim = mlp_dim
+        self.dropout = dropout
 
         self.patch_size = patch_size
         self.d_model = d_model
         self.num_layers = num_layers
 
+        num_patches = (input_shape[0] // patch_size) ** 2
+        self.patch_dim = input_shape[2] * patch_size ** 2
+
         self.pos_emb = self.add_weight(
             "pos_emb", shape=(1, num_patches + 1, d_model)
         )
         self.class_emb = self.add_weight("class_emb", shape=(1, 1, d_model))
+
+        self._build()
+        self.loss_function = tf.losses.CategoricalCrossentropy()
+        self.accuracy_function = tf.keras.metrics.CategoricalAccuracy()
+        with tf.device("/cpu:0"):
+            self(x=tf.constant(tf.zeros(shape=(1,)+input_shape,
+                                             dtype=tf.float32)))
 
     def extract_patches(self, images):
         batch_size = tf.shape(images)[0]
@@ -39,13 +71,13 @@ class VisionTransformer(MyModel):
         self.embedded_patche = tf.keras.layers.Dense(self.d_model)
 
         self.enc_layers = [
-            TransformerBlock(self.d_model, num_heads, mlp_dim, dropout)
+            TransformerBlock(self.d_model, self.num_heads, self.mlp_dim, self.dropout)
             for _ in range(self.num_layers)
         ]
 
         self.ln1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
-        self.dense1 = tf.keras.layers.Dense(mlp_dim, activation='gelu')
-        self.dropout1 = tf.keras.layers.Dropout(dropout)
+        self.dense1 = tf.keras.layers.Dense(self.mlp_dim, activation=gelu)
+        self.dropout1 = tf.keras.layers.Dropout(self.dropout)
         self.dense2 = tf.keras.layers.Dense(self.out_dim, activation='softmax')
 
 
@@ -64,12 +96,25 @@ class VisionTransformer(MyModel):
         for layer in self.enc_layers:
             x = layer(x, trainable)
 
-        x = self.ln1(x)
+        x = self.ln1(x[:, 0])
         x = self.dense1(x)
         x = self.dropout1(x)
         x = self.dense2(x)
 
         return x
+
+    def loss(self, logits, answer):
+        return self.loss_function(y_true=answer, y_pred=logits)
+
+    def optimize(self, loss, tape=None):
+        assert tape is not None, 'please set tape in opmize'
+        grads = tape.gradient(loss, self.trainable_variables)
+        self.optimizer.method.apply_gradients(zip(grads, self.trainable_variables))
+        return
+
+    def accuracy(self, logits, answer):
+        self.accuracy_function(y_true=answer, y_pred=logits)
+        return self.accuracy_function.result()
 
 
 class MultiHeadSelfAttention(tf.keras.layers.Layer):
@@ -82,10 +127,10 @@ class MultiHeadSelfAttention(tf.keras.layers.Layer):
                 f"embedding dimension = {embed_dim} should be divisible by number of heads = {num_heads}"
             )
         self.projection_dim = embed_dim // num_heads
-        self.query_dense = Dense(embed_dim)
-        self.key_dense = Dense(embed_dim)
-        self.value_dense = Dense(embed_dim)
-        self.combine_heads = Dense(embed_dim)
+        self.query_dense = tf.keras.layers.Dense(embed_dim)
+        self.key_dense = tf.keras.layers.Dense(embed_dim)
+        self.value_dense = tf.keras.layers.Dense(embed_dim)
+        self.combine_heads = tf.keras.layers.Dense(embed_dim)
 
     def attention(self, query, key, value):
         score = tf.matmul(query, key, transpose_b=True)
@@ -125,16 +170,16 @@ class TransformerBlock(tf.keras.layers.Layer):
         self.att = MultiHeadSelfAttention(embed_dim, num_heads)
         self.mlp = tf.keras.Sequential(
             [
-                Dense(mlp_dim, activation='gelu'),
-                Dropout(dropout),
-                Dense(embed_dim),
-                Dropout(dropout),
+                tf.keras.layers.Dense(mlp_dim, activation=gelu),
+                tf.keras.layers.Dropout(dropout),
+                tf.keras.layers.Dense(embed_dim),
+                tf.keras.layers.Dropout(dropout),
             ]
         )
-        self.layernorm1 = LayerNormalization(epsilon=1e-6)
-        self.layernorm2 = LayerNormalization(epsilon=1e-6)
-        self.dropout1 = Dropout(dropout)
-        self.dropout2 = Dropout(dropout)
+        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = tf.keras.layers.Dropout(dropout)
+        self.dropout2 = tf.keras.layers.Dropout(dropout)
 
     def call(self, inputs, training):
         inputs_norm = self.layernorm1(inputs)
