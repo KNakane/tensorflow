1dd0668688b776ea8d2a01ce01dfb6e443153d67
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-30 22:30:48 +0000

    Adding tarfile member sanitization to extractall()

--- dataset/segment_load.py
+++ dataset/segment_load.py
@@ -27,7 +27,26 @@ class Load():
                 wget.download(url="http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar",
                               out=self.dataset_dir)
             with tarfile.open(self.dataset_dir+'/VOCtrainval_11-May-2012.tar', mode='r:*') as tar:
-                tar.extractall(self.dataset_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, self.dataset_dir)
         return 
 
     def get_data(self):
